643abcd09b38bbadf8d2b134f1fefc862ae58f85
Arthur Benware <artpolitic91@gmail.com>
2023-07-16 18:10:30 -0400

    commit tree f0440877c2fc6c2525f117a8eb3a4e89d661ae8e

---
+++ db/migrate/20230716204241_create_recipes.rb
@@ -0,0 +1,13 @@
+class CreateRecipes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :recipes do |t|
+      t.belongs_to :user 
+      t.string :title
+      t.text :instructions
+      t.integer :minutes_to_complete
+      
+
+      t.timestamps
+    end
+  end
+end
